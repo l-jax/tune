8ed62ffc36afb82e70f708cd28468466e956c149
Laura Jaques <laura-jaques@outlook.com>
2025-06-05 09:23:56 +0100

    refactor suggestion tests

--- vtune_test.go
+++ vtune_test.go
@@ -13,12 +13,22 @@ const (
 
 var testTable = Table{50000, 40000}
 
-var scaleFactorTests = map[string]struct {
+var paramCalculationTests = map[string]struct {
 	daysBetweenVacuums, scaleFactor, vacuumsPerDay float64
 }{
 	"twice a day":    {daysBetweenVacuums: 0.5, vacuumsPerDay: 2.0},
 	"daily":          {daysBetweenVacuums: 1.0, vacuumsPerDay: 1.0},
 	"every two days": {daysBetweenVacuums: 2.0, vacuumsPerDay: 0.5},
+}
+
+var paramSuggestionTests = map[string]struct {
+	table                           Table
+	threshold                       uint64
+	daysBetweenVacuums, scaleFactor float64
+}{
+	"many rows many updates favours scale factor":          {Table{50000, 550}, defaultThreshold, defaultDaysBetweenVacuums, 0.01},
+	"many rows few updates favours threshold":              {Table{50000, 100}, 100, defaultDaysBetweenVacuums, 0.0},
+	"updates less than default threshold lowers threshold": {Table{50000, 12}, 12, defaultDaysBetweenVacuums, 0.0},
 }
 
 func TestNewTableEmpty(t *testing.T) {
@@ -31,59 +41,17 @@ func TestNewTableNoUpdates(t *testing.T) {
 	assertError(t, err, ErrNoUpdates)
 }
 
-func TestSuggestAutovacuumParametersTableWithManyActiveRows(t *testing.T) {
-	table := Table{50000, 550}
-
-	wantScaleFactor := 0.01
-	var wantThreshold uint64 = 50
-
-	params, _ := suggestAutovacuumParameters(table, defaultDaysBetweenVacuums)
-
-	if params.scaleFactor != wantScaleFactor {
-		t.Errorf("scaleFactor = %v, want %v", params.scaleFactor, wantScaleFactor)
-	}
-
-	if params.threshold != wantThreshold {
-		t.Errorf("threshold = %v, want %v", params.threshold, wantThreshold)
-	}
-}
-
-func TestSuggestAutovacuumParametersTableWithFewActiveRows(t *testing.T) {
-	table := Table{50000, 100}
-
-	wantScaleFactor := 0.0
-	var wantThreshold uint64 = 100
-
-	params, _ := suggestAutovacuumParameters(table, defaultDaysBetweenVacuums)
-
-	if params.scaleFactor != wantScaleFactor {
-		t.Errorf("scaleFactor = %v, want %v", params.scaleFactor, wantScaleFactor)
-	}
-
-	if params.threshold != wantThreshold {
-		t.Errorf("threshold = %v, want %v", params.threshold, wantThreshold)
-	}
-}
-
-func TestSuggestAutovacuumParametersTableWithActiveRowsBelowDefaultThreshold(t *testing.T) {
-	table := Table{2134, 12}
-
-	wantScaleFactor := 0.0
-	var wantThreshold uint64 = 12
-
-	params, _ := suggestAutovacuumParameters(table, defaultDaysBetweenVacuums)
-
-	if params.scaleFactor != wantScaleFactor {
-		t.Errorf("scaleFactor = %v, want %v", params.scaleFactor, wantScaleFactor)
-	}
-
-	if params.threshold != wantThreshold {
-		t.Errorf("threshold = %v, want %v", params.threshold, wantThreshold)
+func TestSuggestAutovacuumParameters(t *testing.T) {
+	for name, test := range paramSuggestionTests {
+		t.Run(name, func(t *testing.T) {
+			params, _ := suggestAutovacuumParameters(test.table, test.daysBetweenVacuums)
+			assertParams(t, params, test.scaleFactor, test.threshold)
+		})
 	}
 }
 
 func TestCalculateScaleFactor(t *testing.T) {
-	for name, test := range scaleFactorTests {
+	for name, test := range paramCalculationTests {
 		t.Run(name, func(t *testing.T) {
 			scaleFactor, _ := calculateScaleFactor(testTable, defaultThreshold, test.daysBetweenVacuums)
 			assertVacuumsPerDay(t, defaultThreshold, scaleFactor, test.vacuumsPerDay)
@@ -97,7 +65,7 @@ func TestCalculateScaleFactorNoDaysBetweenVacuums(t *testing.T) {
 }
 
 func TestCalculateThresholdForVacuum(t *testing.T) {
-	for name, test := range scaleFactorTests {
+	for name, test := range paramCalculationTests {
 		t.Run(name, func(t *testing.T) {
 			threshold, _ := calculateThreshold(testTable, defaultScaleFactor, test.daysBetweenVacuums)
 			assertVacuumsPerDay(t, threshold, defaultScaleFactor, test.vacuumsPerDay)
@@ -146,5 +114,15 @@ func assertError(t *testing.T, got error, want error) {
 	t.Helper()
 	if !errors.Is(got, want) {
 		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func assertParams(t *testing.T, params *Params, wantScaleFactor float64, wantThreshold uint64) {
+	if params.scaleFactor != wantScaleFactor {
+		t.Errorf("scaleFactor = %v, want %v", params.scaleFactor, wantScaleFactor)
+	}
+
+	if params.threshold != wantThreshold {
+		t.Errorf("threshold = %v, want %v", params.threshold, wantThreshold)
 	}
 }
